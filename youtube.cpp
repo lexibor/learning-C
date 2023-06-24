62a20af888e4524a13feadf3512ee7f21e6b0e4b
lexibor <brionesornelas.alexia@gmail.com>
2023-06-24 12:09:41 -0500

    started to add comments for explanations

--- youtube.cpp
+++ youtube.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <iostream> //cout is defined in iostream
 
-int main()
+int main() //main function 
 {
     std::cout << "Hello there\n";
 
-    return 0;
+    return 0; //output sometimes has "EXIT_SUCCESS"
 }
