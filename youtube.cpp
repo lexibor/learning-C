a11a2a421f7eb92d14b4d264612477878c88fcf2
lexibor <brionesornelas.alexia@gmail.com>
2023-06-27 11:23:06 -0500

    cout vs printf

--- youtube.cpp
+++ youtube.cpp
@@ -16,5 +16,9 @@ int main() //main function
 
     //int slices = 5;
 
+    std::cout << "You have " << slices << " slices of pizza." << std::endl; //endl goes to new line
+
+    printf("%i\n", slices); //printf has to specify type whereas cout doesnt need the specification
+
     return 0; //output sometimes has "EXIT_SUCCESS"
 }
