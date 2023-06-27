e1a004dce3303475573ff9660852f091eea968be
lexibor <brionesornelas.alexia@gmail.com>
2023-06-27 11:08:12 -0500

    adding comments explaining std::cout.....

--- youtube.cpp
+++ youtube.cpp
@@ -4,5 +4,9 @@ int main() //main function
 {
     std::cout << "Hello there\n";
 
+    //std -> namespace (a grouping of code and prevents naming conflicts)
+    //cout -> object (like console.out) cout comes from class ostream
+    //<< -> an operator (like +, -, /)
+
     return 0; //output sometimes has "EXIT_SUCCESS"
 }
