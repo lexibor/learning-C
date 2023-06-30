606c1013555b8204dac2f0c9bbc220b26e54e305
lexibor <brionesornelas.alexia@gmail.com>
2023-06-30 12:35:17 -0500

    char and integral data types;

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -11,6 +11,36 @@
 
     INTEGRAL
 
-    
-
+    int - can be 16bits(guaranteed) and 32bits
+    bit - 1s and 0s (binary)
+    sign bit - to represent negative numbers
+    signed - can represent negative numbers but max number is halved
+    unsigned - only positive numbers
 */
+
+#include <iostream>
+#include <climits>
+
+int main()
+{
+    // INTEGRAL
+    short a;
+    int b;
+    long c;
+    long long d;
+    unsigned short aa;
+    unsigned int bb;
+    unsigned long cc;
+    unsigned long long dd;
+
+   // std::cout << USHRT_MAX << std::endl;  // adding a 'U' at the beginning shows the unsigned amount
+
+    // CHAR
+    char x = 65; // chars always use single quotes
+
+    std::cout << x << std::endl;
+    std::cout << (int)x << std::endl; // casting to an integer tells you the num equivalent of a char based on ASCII
+
+
+    return 0;
+}
