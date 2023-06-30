27153e70f5c97c5763ed572952a8416e1891f0b7
lexibor <brionesornelas.alexia@gmail.com>
2023-06-30 12:14:17 -0500

    strating data types in c++

---
+++ dataTypes.cpp
@@ -0,0 +1,16 @@
+/*
+    DATA TYPES
+
+    intergal - int
+    char - character
+    floating point - fraction
+    bool - T/F
+    arrays - list of memory
+    structs
+    class - blueprint of a structure
+
+    INTEGRAL
+
+    
+
+*/
