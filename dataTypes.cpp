2ca7ddf8086d4f5a54ea9f249ea8182f9e631b77
lexibor <brionesornelas.alexia@gmail.com>
2023-07-05 14:10:12 -0500

    floating point number types

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -20,6 +20,7 @@
 
 #include <iostream>
 #include <climits>
+#include <float.h>
 
 int main()
 {
@@ -43,16 +44,34 @@ int main()
     std::cout << (int)x << std::endl; // casting to an integer tells you the num equivalent of a char based on ASCII
 
     // BOOL
-    bool pizza_is_good = -1; 
+    bool pizza_is_good = true; 
 
     /*  booleans have a numeric value,
         0 is equal to "false"
         any other number is considered "true" 
         but when cout-ing a boolean value with another number
-        it will always show up as "1"
+        it will always show up as "1" because it is most correlated with "true"
     */
-
    std::cout << pizza_is_good << std::endl;
+
+   // to print out "true" or "false" instead of 1 or 0
+
+   std::cout << std::boolalpha << pizza_is_good << std::endl;
+
+   // FLOATING POINT NUMBERS 
+   
+   float e;
+   double f = 77000; // 7.7E4  7.7 * 10^4
+   long double g;
+
+   /*
+        float has the least number of significant digits - least "trustworthy"
+   */
+  std::cout << FLT_DIG << std::endl; // 6 digits
+  std::cout << DBL_DIG << std::endl; // 15 digits
+  std::cout << LDBL_DIG << std::endl; // 18 digits
+
+
 
 
 }
