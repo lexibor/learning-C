3444dc8dcdb46d9539b66643cd9af414727fe7ef
lexibor <brionesornelas.alexia@gmail.com>
2023-08-15 10:55:42 -0500

    modified some comments

--- hexOctal.cpp
+++ hexOctal.cpp
@@ -7,15 +7,15 @@ int main()
 
     int number = 30; // base 10 outputs 30
 
-    //int number = 0x30; //base 16 outputs 48
+    //int number = 0x30; //base 16 (Hexadecimal) outputs 48
 
-    //int number = 030; //base 8 outputs 24
+    //int number = 030; //base 8 (Octal) outputs 24
 
     std::cout << number << std::endl;
 
     //how to show a specific number in another base
 
-    std::cout << "Base 16: " << std::hex << number << std::endl; // in base 16
+    std::cout << "Base 16: " << std::hex << number << std::endl; // in base 16 AKA Hexadecimal
 
-    std::cout << "Base 8: " << std::oct << number << std::endl; // in base 8
+    std::cout << "Base 8: " << std::oct << number << std::endl; // in base 8 AKA Octal
 }
