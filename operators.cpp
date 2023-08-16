cee406eb72871de11b327a7a7b3549bd2b6de380
lexibor <brionesornelas.alexia@gmail.com>
2023-08-16 07:35:47 -0500

    operators in cpp

--- operators.cpp
+++ operators.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+    int x = 5 + 5; // assignment operator; addition operator
     
+    //common operators are the same as the ones in Java
+    
+    //int y = 10/4 // gives 2 because nums are not doubles
+
+    double y = 10.0 / 4; // now gives double answer (2.5)
+
+    std::cout << y << std::endl;
+
+    double z = 10 % 4; // modulus gives the remainder in a division (2)
+
+    std::cout << z << std::endl;
+
 }
